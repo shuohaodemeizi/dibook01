b66e11c85420c6ee3255695ba855508d21dc3444
chenzhuobin <chenzhuobin>
2018-11-27 16:41:22 +0800

    composer update 内存不足

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        ini_set('memory_limit','512M');
         //
         Schema::defaultStringLength(191);
     }
