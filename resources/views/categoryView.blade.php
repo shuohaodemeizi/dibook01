b8c47f416a817fc411ecbb80062d1e0053e8f7be
chenzb <>
2019-12-13 15:04:27 +0800

    '新版吧'

--- resources/views/categoryView.blade.php
+++ resources/views/categoryView.blade.php
@@ -14,25 +14,22 @@
 @section('privateContent')
 
 
-    <ul id="Huifold1" class="Huifold1111111">
+<ul id="Huifold1" class="Huifold1111111">
     @foreach($list->categorys as $info)
+          <li class="item">
+            <h4>{{$info->name}}<b>+</b></h4>
+            <div class="info" >
+                @foreach($info->products as $product)
+                  <a href="{{$product->url}}" target="_blank" title="{{$product->name}}">{{$product->name}}</a> <br>
 
-  <li class="item">
-    <h4>{{$info->name}}<b>+</b></h4>
-    <div class="info" >
-        @foreach($info->products as $product)
-          <a href="{{$product->url}}" target="_blank" title="{{$product->name}}">{{$product->name}}</a> <br>
-
-          @isset($product->tags)
-          @foreach($product->tags as $tag)
-              <span class="pull-right"><a href="/t/{{$tag->id}}.html" class=" badge badge-secondary radius">{{$tag->name}}</a></span>
-          @endforeach
-          @endisset
-        @endforeach
-    </div>
-  </li>
-
-
+                  @isset($product->tags)
+                  @foreach($product->tags as $tag)
+                      <span class="pull-right"><a href="/t/{{$tag->id}}.html" class=" badge badge-secondary radius">{{$tag->name}}</a></span>
+                  @endforeach
+                  @endisset
+                @endforeach
+            </div>
+          </li>
     @endforeach
 </ul>
 
